40848d41a0287a8c80b6dddbf25b00f92f286d86
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-10-09 14:43:25 -0700

    notes on the setup file. This explains what we are referencing.

--- js-pacman-START-HERE/setup.js
+++ js-pacman-START-HERE/setup.js
@@ -4,7 +4,7 @@ export const DIRECTIONS = {
   ArrowLeft: {
     code: 37,
     movement: -1,
-    rotation: 180
+    rotation: 180//This Rotates pacman to go the oppsite direction
   },
   ArrowUp: {
     code: 38,
@@ -24,22 +24,22 @@ export const DIRECTIONS = {
 };
 
 export const OBJECT_TYPE = {
-  BLANK: 'blank',
-  WALL: 'wall',
-  DOT: 'dot',
-  BLINKY: 'blinky',
-  PINKY: 'pinky',
-  INKY: 'inky',
-  CLYDE: 'clyde',
-  PILL: 'pill',
-  PACMAN: 'pacman',
+  BLANK: 'blank',//the structure
+  WALL: 'wall',//the structure
+  DOT: 'dot',//the structure
+  BLINKY: 'blinky',//Ghosts
+  PINKY: 'pinky',//Ghosts
+  INKY: 'inky',//Ghosts
+  CLYDE: 'clyde',//Ghosts
+  PILL: 'pill',// the dots
+  PACMAN: 'pacman',//pacman himsef
   GHOST: 'ghost',
-  SCARED: 'scared',
-  GHOSTLAIR: 'lair'
+  SCARED: 'scared',//Scared ghosty
+  GHOSTLAIR: 'lair'//ghostlair??
 };
 
 // Lookup array for classes
-export const CLASS_LIST = [
+export const CLASS_LIST = [//This is set up to work with the "level" object. Its set to the array number
   OBJECT_TYPE.BLANK,
   OBJECT_TYPE.WALL,
   OBJECT_TYPE.DOT,
@@ -53,7 +53,7 @@ export const CLASS_LIST = [
 ];
 
 // prettier-ignore
-export const LEVEL = [
+export const LEVEL = [//This referes to the array above. 1 is blank, 2 is wall, etc. See the above. We reference the array above and put it here to build it out. 
   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
   1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1,
   1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1,
